9d8de06dcf6e13f3c25a5b148d307aa6ee35b80d
emranco <emranhadad770774255770774255@gmail.com>
2022-04-10 02:07:31 +0300

    Add login_user

--- resources/views/user/components/topbar.blade.php
+++ resources/views/user/components/topbar.blade.php
@@ -163,7 +163,6 @@
             <a class="dropdown-item" href="#">
                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Others Profile
             </a>
-
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout
